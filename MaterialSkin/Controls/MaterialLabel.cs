3d731756b945714e5bfe0ae331c9c9b4475e0ba2
Hawx <Hawxy@users.noreply.github.com>
2017-02-05 23:08:47 +0800

    Smooth font rendering + disable forced font

--- MaterialSkin/Controls/MaterialLabel.cs
+++ MaterialSkin/Controls/MaterialLabel.cs
@@ -1,24 +1,50 @@
 ﻿using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Text;
 using System.Windows.Forms;
+
 
 namespace MaterialSkin.Controls
 {
     public class MaterialLabel : Label, IMaterialControl
     {
+        private Font _font;
+
+        public MaterialLabel()
+        {
+            _font = SkinManager.ROBOTO_MEDIUM_11;
+        }
+
         [Browsable(false)]
         public int Depth { get; set; }
         [Browsable(false)]
         public MaterialSkinManager SkinManager => MaterialSkinManager.Instance;
         [Browsable(false)]
         public MouseState MouseState { get; set; }
+        [DefaultValue("")]
+        public override Font Font
+        {
+            get { return _font; }
+            set { _font = value; }
+        }
+        
         protected override void OnCreateControl()
         {
             base.OnCreateControl();
 
             ForeColor = SkinManager.GetPrimaryTextColor();
-            Font = SkinManager.ROBOTO_REGULAR_11;
+  
+            FontChanged += (sender, args) => Font = _font;
 
             BackColorChanged += (sender, args) => ForeColor = SkinManager.GetPrimaryTextColor();
+        }
+
+        public TextRenderingHint TextRenderingHint { get; set; } = TextRenderingHint.SystemDefault;
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.TextRenderingHint = TextRenderingHint;
+            base.OnPaint(e);
         }
     }
 }
